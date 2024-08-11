3f6013ae3d67eba2ecb609d144ad4e8a3dea44a1
deansrebel <alvayana01@gmail.com>
2024-08-11 21:07:42 +0300

    All locations and pics

--- Assets/Scripts/Step.cs
+++ Assets/Scripts/Step.cs
@@ -9,7 +9,11 @@ public class Step : MonoBehaviour
     [TextArea(5,10)]
     [SerializeField] private string _answers;
     [SerializeField] private string _location;
-    [SerializeField] private Step _nextStep;
+    
+    [SerializeField] private Step[] _nextSteps;
+    
+    [SerializeField] private Sprite []_background;
+    [SerializeField] private  Sprite _newSprite;
     
 
     #endregion
@@ -19,7 +23,7 @@ public class Step : MonoBehaviour
     public string Answer => _answers;
     public string Description => _description;
     public string Location => _location;
-    public Step NextStep => _nextStep;
+    public Step[] NextSteps => _nextSteps;
 
     #endregion
 }
--- Assets/Scripts/TextQuest.cs
+++ Assets/Scripts/TextQuest.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TextQuest : MonoBehaviour
@@ -10,25 +11,59 @@ public class TextQuest : MonoBehaviour
     [SerializeField] private TMP_Text _locationLabel;
     [SerializeField] private Step _startStep;
 
+    private Step _currentStep;
+    
+
     #endregion
 
     #region Unity lifecycle
 
     private void Start()
     {
-        _locationLabel.text = _startStep.Location;
-        _descriptionLabel.text = _startStep.Description;
-        _answerLabel.text = _startStep.Answer;
+        SetCurrentStepAndUpdateUi(_startStep);
+    }
+
+    private void SetCurrentStepAndUpdateUi(Step step)
+    {
+        _currentStep = step;
+        UpdateUI();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        for (int i = 1; i <= 9; i++)
         {
+            if (Input.GetKeyDown(i.ToString()))
+            {
+                TryToNextStep(i);
+            }
+            
+            else if (Input.GetKeyDown(KeyCode.Space)) 
+            {
             _locationLabel.text = "Дом";
-            _descriptionLabel.text = "GAME OVER! Приключения тебя ждали, а ты не пошел ТТ.";
-            _answerLabel.text = string.Empty;
+            _descriptionLabel.text = "GAME OVER! Приключения тебя ждали, а ты не пошел ТТ";
+            _answerLabel.text = string.Empty; 
+            }
         }
+    }
+
+    private void TryToNextStep(int number)
+    {
+        int nextStepsCount = _currentStep.NextSteps.Length;
+        if (number > nextStepsCount)
+        {
+            return;
+        }
+        int nextStepIndex = number - 1;
+        Step nextSteps = _currentStep.NextSteps[nextStepIndex];
+        SetCurrentStepAndUpdateUi(nextSteps);
+    }
+
+    private void UpdateUI()
+    {
+        _descriptionLabel.text = _currentStep.Description;
+        _answerLabel.text = _currentStep.Answer;
+        _locationLabel.text = _currentStep.Location;
     }
 
     #endregion
